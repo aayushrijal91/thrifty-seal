309fab06bc51a8d18d5c4374a06fe8ec744868ab
Aayush Rijal <aayush.rijal99@gmail.com>
2022-06-09 17:13:36 +1000

    WIP problem-solved

--- src/theme-files/page-templates/front-page.php
+++ src/theme-files/page-templates/front-page.php
@@ -6,6 +6,8 @@
  */
 get_header();
 ?>
-<?php
-get_footer();
-?>
+
+<?php get_template_part('parts/section', 'solutions'); ?>
+<?php get_template_part('parts/section', 'compare-slider'); ?>
+
+<?php get_footer(); ?>
