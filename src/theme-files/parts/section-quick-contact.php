08667e6802f79e391a490d07b2370336a71d55a9
Aayush Rijal <aayush.rijal99@gmail.com>
2022-06-15 11:14:56 +1000

    Quick contact done

--- src/theme-files/parts/section-quick-contact.php
+++ src/theme-files/parts/section-quick-contact.php
@@ -1,7 +1,23 @@
 <section class="quick-contact">
     <div class="container">
         <div class="quick-contact-main">
-            <div class="text-center text-white heroHeading">Don't miss out on a FREE ONSITE QUOTE</div>
+            <div class="heroHeading">Don't miss out on a FREE ONSITE QUOTE</div>
+            <div class="subHeading">We come to you & give you an on-the-spot assessment & price with no hidden extras!</div>
+            <div class="row justify-content-center pt-5">
+                <div class="col-md-9 col-lg-7">
+                    <div class="row no-gutters">
+                        <div class="col-4 px-2">
+                            <a href="" class="btn btn-block btn-dark">Get Quote</a>
+                        </div>
+                        <div class="col-4 px-2">
+                            <a href="" class="btn btn-block btn-darker">Call Now</a>
+                        </div>
+                        <div class="col-4 px-2">
+                            <a href="" class="btn btn-block btn-black">Book Online</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </section>
