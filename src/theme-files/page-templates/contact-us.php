1c57cc589f1cc17c3518e719b988e9de8a0e045e
Aayush Rijal <aayush.rijal99@gmail.com>
2022-06-15 17:02:48 +1000

    WIP contact us page

--- src/theme-files/page-templates/contact-us.php
+++ src/theme-files/page-templates/contact-us.php
@@ -8,6 +8,16 @@ get_header();
 get_template_part('parts/section', 'banner');
 ?>
 
+<section class="contact-us">
+    <div class="container">
+        <div class="contact-us-wrapper">
+            <div class="form_heading">Booking Form</div>
+            <div class="text-darker-grey h14 pt-3">Simply complete our form below & we’ll get back to you as soon as possible</div>
+            <?= do_shortcode('[contact-form-7 id="793" title="Contact Us"]') ?>
+        </div>
+    </div>
+</section>
+
 <?php
 get_template_part('parts/section', 'compare-slider');
 get_footer();
