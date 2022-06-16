2cc733bf5e1a76b963705063ab48d0bbd4716695
Aayush Rijal <aayush.rijal99@gmail.com>
2022-06-16 10:51:33 +1000

    Gallery page mobile screen done

--- src/theme-files/header.php
+++ src/theme-files/header.php
@@ -22,14 +22,14 @@ $logo = get_field('logo', 'options');
     <header class="header">
         <div class="header-top-bar">
             <div class="container-fluid">
-                <div class="row justify-content-center justify-content-md-between align-items-center pl-md-4">
-                    <div class="col-auto col-xxl">
+                <div class="row justify-content-center justify-content-lg-between align-items-center pl-lg-4">
+                    <div class="col-auto col-xxl pt-4 pt-lg-0">
                         <a class="navbar-brand" href="<?= home_url() ?>"><img src="<?= $logo['url']; ?>" alt="<?= $logo['alt']; ?>" /></a>
                     </div>
                     <div class="col-md-auto pt-4 pt-md-0">
-                        <div class="row no-gutters align-items-center justify-content-center">
-                            <div class="col-md-auto py-2 py-md-0">
-                                <div class="text-uppercase text-white fw-600 h27 px-md-4">
+                        <div class="row no-gutters align-items-center justify-content-center justify-content-md-between">
+                            <div class="col-md-6 col-lg-auto py-2 py-md-4 py-lg-0">
+                                <div class="text-uppercase text-white fw-600 h27 px-lg-4">
                                     <div class="row no-gutters align-items-center">
                                         <div class="col-auto">
                                             <img src="<?= get_template_directory_uri() ?>/images/icons/header-top-dollar.png" class="pr-3">
@@ -38,8 +38,8 @@ $logo = get_field('logo', 'options');
                                     </div>
                                 </div>
                             </div>
-                            <div class="col-md-auto py-2 py-md-0">
-                                <div class="text-uppercase text-white fw-600 h27 px-md-5">
+                            <div class="col-md-5 col-lg-auto py-2 py-md-4 py-lg-0">
+                                <div class="text-uppercase text-white fw-600 h27 px-lg-5">
                                     <div class="row no-gutters align-items-center">
                                         <div class="col-auto">
                                             <img src="<?= get_template_directory_uri() ?>/images/icons/header-top-hrs.png" class="pr-3">
@@ -48,13 +48,13 @@ $logo = get_field('logo', 'options');
                                     </div>
                                 </div>
                             </div>
-                            <div class="col-6 col-md-auto py-3 py-md-0">
+                            <div class="col-6 col-md-4 col-lg-auto py-3 py-md-0">
                                 <a href="tel: <?= get_field('phone_number', 'options') ?>" class="btn btn-block btn-black text-white fw-800 px-4">$0 Call Out Fee</a>
                             </div>
-                            <div class="col-6 col-md-auto py-3 py-md-0">
+                            <div class="col-6 col-md-4 col-lg-auto py-3 py-md-0">
                                 <a href="tel: <?= get_field('phone_number', 'options') ?>" class="btn btn-block btn-primary text-white fw-800 px-4"><?= get_field('phone_number', 'options') ?></a>
                             </div>
-                            <div class="col-auto pl-md-4 pr-2 pt-3 pb-4 py-md-0">
+                            <div class="col-auto col-md-4 col-lg-auto pl-md-4 pr-2 pt-3 pb-4 py-md-0">
                                 <div class="row no-gutters align-items-center">
                                     <div class="col-auto">
                                         <?php if (have_rows('social_media', 'option')) : ?>
@@ -74,7 +74,7 @@ $logo = get_field('logo', 'options');
             </div>
         </div>
         <div class="header-bottom-bar">
-            <div class="container">
+            <div class="container px-md-0 px-lg-3">
                 <nav class="navbar navbar-expand-xl navbar-light p-0 px-xxl-5">
                     <a href="tel: <?= get_field('phone_number', 'options') ?>" class="btn btn-primary">Get a free on-site quote</a>
                     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#baywashMainNav" aria-controls="baywashMainNav" aria-expanded="false" aria-label="Toggle navigation">
