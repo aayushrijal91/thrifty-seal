d36c09c9eafd0717d4c46f91128fae6efa8c9cb8
Aayush Rijal <aayush.rijal99@gmail.com>
2022-06-17 11:26:24 +1000

    Ready for staging

--- src/theme-files/parts/section-compare-slider.php
+++ src/theme-files/parts/section-compare-slider.php
@@ -1,24 +1,19 @@
 <section class="compare-slider">
     <div class="container-fluid">
         <div class="row no-gutters justify-content-center">
-            <div class="col-12 col-md-6 col-xl-4 py-2 p-md-3 py-xl-0">
-                <div class="cocoen">
-                    <img src="https://permaseal.net.au/assets/images/lib/before-1.jpg" alt="" class="before-img">
-                    <img src="https://permaseal.net.au/assets/images/lib/after-1.jpg" alt="" class="after-img">
-                </div>
-            </div>
-            <div class="col-12 col-md-6 col-xl-4 py-2 p-md-3 py-xl-0">
-                <div class="cocoen">
-                    <img src="https://permaseal.net.au/assets/images/lib/before-1.jpg" alt="" class="before-img">
-                    <img src="https://permaseal.net.au/assets/images/lib/after-1.jpg" alt="" class="after-img">
-                </div>
-            </div>
-            <div class="col-12 col-md-6 col-xl-4 py-2 p-md-3 py-xl-0">
-                <div class="cocoen">
-                    <img src="https://permaseal.net.au/assets/images/lib/before-1.jpg" alt="" class="before-img">
-                    <img src="https://permaseal.net.au/assets/images/lib/after-1.jpg" alt="" class="after-img">
-                </div>
-            </div>
+            <?php if (have_rows('gallery', 'option')) :
+                for ($i = 0; $i < 3; $i++) : the_row();
+            ?>
+                    <div class="col-12 col-md-6 col-xl-4 py-2 p-md-3 py-xl-0">
+                        <div class="cocoen">
+                            <img src="<?= get_sub_field('before_image')['url'] ?>" alt="<?= get_sub_field('before_image')['alt'] ?>" class="before-img">
+                            <img src="<?= get_sub_field('after_image')['url'] ?>" alt="<?= get_sub_field('after_image')['alt'] ?>" class="after-img">
+                        </div>
+                    </div>
+            <?php
+                endfor;
+            endif;
+            ?>
         </div>
     </div>
 </section>
